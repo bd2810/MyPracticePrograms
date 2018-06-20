3544c3767c04a870e49a4d251164e6b2f24a315b
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-06-20 14:41:13 -0400

    added reverse string program

--- src/programs/ReverseString.java
+++ src/programs/ReverseString.java
@@ -1,10 +1,38 @@
 package programs;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scan;
 
+	public static void main(String[] args) {
+		
+		scan = new Scanner(System.in);
+		
+		String str = scan.next();
+		
+		System.out.println(reverse(str));		
+
+	}
+	
+	public static String reverse(String str) {
+
+		char[] chs = str.toCharArray();
+
+		int i = 0, j = chs.length - 1;
+
+		while (i < j) {
+
+			char t = chs[i];
+			chs[i] = chs[j];
+			chs[j] = t;
+			i++;
+			j--;
+
+		}
+
+		return String.valueOf(chs);
 	}
 
 }
