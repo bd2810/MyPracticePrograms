b027c2d35961da32bd24080ba1294a41e0dfcdb9
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-06-23 00:08:09 -0400

    added merge and sort list of strings program

---
+++ General/MergeAndSortStrings.java
@@ -0,0 +1,45 @@
+/*
+ * Given a list of Strings, merge all the Strings in a single string in a sorted order.
+ * Input: ["acb", "dab", "hcd"]
+ * Output: "aabbccddh"
+ */
+
+package programs;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeAndSortStrings {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("acb", "dab", "hcd");
+		
+		System.out.println(mergeAndSort(list));
+	}
+	
+		
+	public static String mergeAndSort (List<String> list) {
+		
+		String finalString = null;
+		
+		StringBuilder sBuilder = new StringBuilder();
+		
+		for (int i=0; i<list.size(); i++) {
+			
+			sBuilder.append(list.get(i));
+			
+		}
+		
+		String str = sBuilder.toString();
+		
+		char[] ch = str.toCharArray();
+		
+		Arrays.sort(ch);
+		
+		finalString = String.valueOf(ch);
+				
+		return finalString;
+	}
+
+}
