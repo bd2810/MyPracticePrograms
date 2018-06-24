fbe386550aed5af3694553da8dc5de9fb7c8d7c1
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-06-24 16:36:40 -0400

    added program to check if one string is permutation of another

--- PracticePrograms/arraysAndStrings/CheckPermutation.java
+++ PracticePrograms/arraysAndStrings/CheckPermutation.java
@@ -1,11 +1,42 @@
+/*
+ * Program to check if two strings are permutation of each other
+ * e.g. "apple" and "papel" are permutation of each i.e. have same characters
+ * e.g. "hello"and "llloh" are not permutation of each other
+ */
+
 package arraysAndStrings;
 
+import java.util.Arrays;
 
 public class CheckPermutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String str1 = "apple";
+		String str2 = "paple";		
+		System.out.println(checkPermutation(str1, str2));
 	}
 
+	//Time complexity = O(1). Space complexity = O(n)
+	public static boolean checkPermutation(String string1, String string2) {
+		
+		boolean result = false;
+		
+		if (string1.length() != string2.length()) {
+			return false;
+		}
+		
+		char[] c1 = string1.toCharArray();
+		char[] c2 = string2.toCharArray();
+		
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		
+		if (Arrays.equals(c1, c2)) {
+			
+			result = true;		
+		}		
+		
+		return result;
+	}
 }
