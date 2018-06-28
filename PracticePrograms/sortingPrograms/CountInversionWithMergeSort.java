6584582568a3a12949c2057e834b52bbcb3fe800
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-06-28 17:34:10 -0400

    updated count inversion program

--- PracticePrograms/sortingPrograms/CountInversionWithMergeSort.java
+++ PracticePrograms/sortingPrograms/CountInversionWithMergeSort.java
@@ -7,93 +7,76 @@
 package sortingPrograms;
 
 public class CountInversionWithMergeSort {
-	
+
 	public static void main(String[] args) {
-		
-		int[] arr = {2,3,1,5,4,8,6};
-		
-		//System.out.println(countInversionSimple(arr));
-		System.out.println(mergeSort(arr, 0, arr.length));
+
+		int[] arr = new int[] { 2, 3, 1, 5, 4, 8, 6 };
+
+		System.out.println("Number of inversions are: " + countInversionSimple(arr));
+		System.out.println("Number of inversions are: " + mergeSort(arr, 0, arr.length - 1));
 	}
-	
+
 	// Method 1: This is simple implementation using for loops with time complexity O(n^2)
 	public static int countInversionSimple(int[] array) {
-		
+
 		int count = 0;
-		
-		for (int i=0; i<array.length; i++) {
-			
-			for (int j=i+1; j<array.length; j++) {
-				
+
+		for (int i = 0; i < array.length; i++) {
+
+			for (int j = i + 1; j < array.length; j++) {
+
 				if (array[i] > array[j]) {
-					
+
 					count += 1;
 				}
 			}
 		}
-		
+
 		return count;
-		
+
 	}
-	
-	// Method 2: With Divide and Conquer using Merge Sort. Time Complexity = O(n log n)
-	public static int mergeSort(int[] array, int left, int right) {
-		
-		int count = 0;		
-		
+
+	// Method 2: Using Divide and Conquer (MergeSort). Time complexity = O(n log n)
+	static int mergeSort(int arr[], int left, int right) {
+
+		int mid;
+		int finalCount = 0;
+
 		if (right > left) {
-			
-			int middle = (right+left)/2;
-			
-			count += mergeSort(array, left, middle);
-			count += mergeSort(array, middle+1, right);
-			
-			count += merge(array, left, middle+1, right);
-		}		
-		
+
+			mid = (right + left) / 2;
+
+			// Recursively call mergeSort for left and right sub-arrays
+			finalCount += mergeSort(arr, left, mid);
+			finalCount += mergeSort(arr, mid + 1, right);
+
+			/* Merge the two parts */
+			finalCount += merge(arr, left, mid + 1, right);
+		}
+		return finalCount;
+	}
+
+	/*
+	 * This method merges two sorted arrays and returns inversion count in the
+	 * arrays.
+	 */
+	static int merge(int arr[], int left, int mid, int right) {
+		int i, j;
+		int count = 0;
+
+		i = left; /* i is index for left subarray */
+		j = mid; /* j is index for right subarray */
+		while ((i <= mid - 1) && (j <= right)) {
+			if (arr[i] <= arr[j]) {
+				i++;
+
+			} else {
+				j++;
+				count = count + (mid - i);
+			}
+		}
+
 		return count;
 	}
-	
-	private static int merge(int[] array, int left, int middle, int right) {
-		
-		int count = 0;
-		
-		int[] tmp = new int[array.length];
-		
-		int i = left;
-		int j= right;
-		int k = left;
-		
-		while ((i <= middle-1) && (j <= right)) {
-			
-			if (array[i] <= array[j]) {
-				tmp[k] = array[i];
-				i++;
-				k++;	
-				
-			} else {
-				tmp[k] = array[j];
-				j++;
-				k++;
-				
-			}			
-		}
-		
-		count += middle - i;
-		
-		while (i <= middle - 1) {
-			tmp[k] = array[i];
-			k++;
-			i++;
-		}
-		
-		while (j <= right) {
-			tmp[k] = array[j];
-			k++;
-			j++;
-		}
-		
-		return count;	
-		
-	}
+
 }
