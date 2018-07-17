94a11464e1bed8d09f2eb73bc18ad6748ce6e1c5
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-07-17 12:08:26 -0400

    added new program for merge string with alternating characters

---
+++ PracticePrograms/arraysAndStrings/MergeStringAlternateChar.java
@@ -0,0 +1,55 @@
+/*
+ * Program to merge two string taking alternate characters from each strings
+ * e.g. Strings 'Pineapple' and 'Test' ---> 'PTiensetapple'
+ * e.g. Strings 'Hello' and 'World' ---> 'HWeolrllod'
+ * e.g. Strings 'Test' and 'Pineapple' ---> 'TPeisnteapple'
+ */
+
+package arraysAndStrings;
+
+public class MergeStringAlternateChar {
+
+	public static void main(String[] args) {
+		
+		String a = "Test";
+		String b = "Pineapple";
+				
+		System.out.println("New string is: " +mergeStrings(a, b));
+
+	}
+	
+	public static String mergeStrings(String a, String b) {
+		
+		String result = "";
+		
+		int i = 0;
+		
+		while (i < a.length() && i < b.length()) {
+			
+			result += a.charAt(i);
+			result += b.charAt(i);
+			i++;
+		}
+		
+		if (a.length() > b.length()) {
+			
+			while (i < a.length()) {
+				result += a.charAt(i);
+				i++;
+			}		
+			
+		} else if (a.length() < b.length()) {
+			
+			while (i < b.length()) {
+				result += b.charAt(i);
+				i++;
+			}
+			
+		} else {
+			return result;
+		}
+		
+		return result;
+	}
+
+}
