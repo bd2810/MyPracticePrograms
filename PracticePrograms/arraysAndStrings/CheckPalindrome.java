f6ee3c233634223419b626e7c718a3a42f926515
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-07-21 00:17:47 -0400

    add program isPalindrome

---
+++ PracticePrograms/arraysAndStrings/CheckPalindrome.java
@@ -0,0 +1,65 @@
+package arraysAndStrings;
+/*
+ * Write a method to find out if there exists one combination with the given
+ * sequence of characters in a String which is palindrome. Return true if
+ * exists else return false
+ * 
+ * eg. - Input = “aabbc” - Output = true (because with above sequence of
+ * characters we can form “abcba” string which is a palindrome)
+ * 
+ * or aaaabbbb -> aabbbbaa
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckPalindrome {
+
+	public static void main(String[] args) {
+
+		String str = "aaaabbbbccd";
+		
+		System.out.println("Is string palindrome: " + checkPalindorme(str));
+
+	}
+
+	public static boolean checkPalindorme(String str) {
+
+		boolean isPalindrome = false;
+		int dub = 0;
+		char[] chr = str.toCharArray();
+
+		Map<Character, Integer> values = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < chr.length; i++) {
+
+			if (values.containsKey(chr[i])) {
+
+				values.put(chr[i], values.get(chr[i]) + 1);
+
+			} else {
+
+				values.put(chr[i], 1);
+
+			}
+		}
+		
+		System.out.println(values);
+
+		for (Map.Entry<Character, Integer> e : values.entrySet()) {
+
+			if (e.getValue() % 2 == 0) {
+
+				dub += 1;
+			}
+		}
+
+		if ((values.size() == dub) || (values.size() == dub+1)) {
+
+			isPalindrome = true;
+		}
+
+		return isPalindrome;
+	}
+
+}
