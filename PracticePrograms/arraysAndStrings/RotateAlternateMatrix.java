f5340123433f6eb1e6c9ed7c1a56c521ffbc0845
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-07-24 12:32:29 -0400

    update matrix

--- PracticePrograms/arraysAndStrings/RotateAlternateMatrix.java
+++ PracticePrograms/arraysAndStrings/RotateAlternateMatrix.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SpiralMatrix {
+public class RotateAlternateMatrix {
 
 	public static void main(String[] args) {
 		
@@ -13,31 +13,31 @@ public class SpiralMatrix {
 				{3, 4, 5, 6 },
 				{7, 8, 9, 10, 11 }};
 		
-		System.out.println(spiral(matrix));
+		System.out.println(rotate(matrix));
 	}
 				
 
-	public static List<Integer> spiral(int[][] matrix) {
+	public static List<Integer> rotate(int[][] matrix) {
 
-		List<Integer> spiralList = new ArrayList<Integer>();
+		List<Integer> rotateList = new ArrayList<Integer>();
 
-		for (int i = 0; int j=0; i < matrix.length; i++) {
+		for (int i = 0; i < matrix.length; i++) {
 
 			if (i % 2 == 0) {
 
 				for (int j = 0; j < matrix[i].length; j++) {
 
-					spiralList.add(matrix[i][j]);
+					rotateList.add(matrix[i][j]);
 				}
 			} else {
 				for (int j = matrix[i].length-1; j >=0 ; j--) {
 
-					spiralList.add(matrix[i][j]);
+					rotateList.add(matrix[i][j]);
 				}
 			}
 		}
 
-		return spiralList;
+		return rotateList;
 
 	}
 
