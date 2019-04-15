2d2fade9ff01761c111ab049efaeee7b8874329e
bd2810 <33436672+bd2810@users.noreply.github.com>
2019-04-15 13:45:48 -0400

    add hashmap method to TwoSum solution

--- PracticePrograms/arraysAndStrings/TwoSum.java
+++ PracticePrograms/arraysAndStrings/TwoSum.java
@@ -1,6 +1,7 @@
 package arraysAndStrings;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -16,7 +17,7 @@ public class TwoSum {
 
 	}
 
-	public static void twoSum(int[] array, int target) {
+	public static int[] twoSum(int[] array, int target) {
 
 		int[] result = new int[2];
 
@@ -35,6 +36,31 @@ public class TwoSum {
 		}
 
 		System.out.println("Two sum is: " + Arrays.toString(result));
+		return result;
+	}
+	
+	static int[] newTwoSum (int target, int[] array) {
+		
+		int[] result = new int[2];
+		
+		HashMap<Integer, Integer> values = new HashMap<>();
+		
+		for (int i=0; i<array.length; i++) {
+			
+			Integer value = values.get(target - array[i]);
+			
+			if (value == null) {
+				
+				values.put(array[i], i);
+				
+			} else {
+				
+				array[0] = value;
+				array[1] = i;
+			}
+		}	
+		
+		return result;
 	}
 
 }
